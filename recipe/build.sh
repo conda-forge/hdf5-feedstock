f299a09c7b3d7b0e944d96e7141381757570445b
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-03-31 00:51:02 -0400

    recipe: Specify prefix to use for finding zlib. [skip appveyor]

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX \
-    --enable-linux-lfs --with-zlib \
-    --with-pthread=yes  --enable-cxx --with-default-plugindir=$PREFIX/lib/hdf5/plugin
+./configure --prefix="${PREFIX}" \
+    --enable-linux-lfs \
+    --with-zlib="${PREFIX}" \
+    --with-pthread=yes  --enable-cxx \
+    --with-default-plugindir="${PREFIX}/lib/hdf5/plugin"
 
 make
 make check
