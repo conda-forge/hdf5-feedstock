2c3e43853725dad295e61cd108db78191841810d
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-06-11 14:11:28 -0400

    recipe: Use the toolchain.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
+#
+source activate "${CONDA_DEFAULT_ENV}"
+
+if [ "$(uname)" == "Darwin" ]
+then
+    export CXX="${CXX} -stdlib=libc++"
+fi
+
 ./configure --prefix="${PREFIX}" \
     --enable-linux-lfs \
     --with-zlib="${PREFIX}" \
