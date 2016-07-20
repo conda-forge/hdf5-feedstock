344e1f4ed120802d398697e2283509bf5cdb3a37
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-07-20 14:22:04 -0400

    Fix paths for tests

--- recipe/test_compilers.sh
+++ recipe/test_compilers.sh
@@ -3,15 +3,15 @@ set -e
 
 # Test C compiler
 echo "Testing h5cc"
-h5cc tests/h5_cmprss.c -o tests/h5_cmprss
-./tests/h5_cmprss
+h5cc $RECIPE_DIR/tests/h5_cmprss.c -o h5_cmprss
+./h5_cmprss
 
 # Test C++ compiler
 echo "Testing h5c++"
-h5c++ tests/h5tutr_cmprss.cpp -o tests/h5tutr_cmprss
-./tests/h5tutr_cmprss
+h5c++ $RECIPE_DIR/tests/h5tutr_cmprss.cpp -o h5tutr_cmprss
+./h5tutr_cmprss
 
 # Test Fortran compiler
 echo "Testing h5fc"
-h5fc tests/h5_cmprss.f90 -o h5_cmprss
-./tests/h5_cmprss
+h5fc $RECIPE_DIR/tests/h5_cmprss.f90 -o h5_cmprss
+./h5_cmprss
