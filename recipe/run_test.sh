e6b42a3e6ee1fc005f4ed5d65a47cb2e897fc7fb
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-07-20 21:10:02 -0400

    Move test files and avoid using $RECIPE_DIR

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,15 +3,15 @@ set -e
 
 # Test C compiler
 echo "Testing h5cc"
-h5cc $RECIPE_DIR/tests/h5_cmprss.c -o h5_cmprss
+h5cc h5_cmprss.c -o h5_cmprss
 ./h5_cmprss
 
 # Test C++ compiler
 echo "Testing h5c++"
-h5c++ $RECIPE_DIR/tests/h5tutr_cmprss.cpp -o h5tutr_cmprss
+h5c++ h5tutr_cmprss.cpp -o h5tutr_cmprss
 ./h5tutr_cmprss
 
 # Test Fortran compiler
 echo "Testing h5fc"
-h5fc $RECIPE_DIR/tests/h5_cmprss.f90 -o h5_cmprss
+h5fc h5_cmprss.f90 -o h5_cmprss
 ./h5_cmprss
--- recipe/tests/README.md
+++
@@ -1,6 +0,0 @@
-About
-=====
-
-The examples in this directory are used to test the installation, and are
-copied from the HDF5 library. The copyright and license for these files is
-contained in the LICENSE.md file.
