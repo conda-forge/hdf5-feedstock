b89c9dcdcdcd66c00831627f0d59a61696afae05
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-08-07 10:31:46 +0100

    Run compiler wrappers with -show

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,15 +3,18 @@ set -e
 
 # Test C compiler
 echo "Testing h5cc"
+h5cc -show
 h5cc h5_cmprss.c -o h5_cmprss
 ./h5_cmprss
 
 # Test C++ compiler
 echo "Testing h5c++"
+h5c++ -show
 h5c++ h5tutr_cmprss.cpp -o h5tutr_cmprss
 ./h5tutr_cmprss
 
 # Test Fortran compiler
 echo "Testing h5fc"
+h5fc -show
 h5fc h5_cmprss.f90 -o h5_cmprss
 ./h5_cmprss
