924ae84bfcb2e29651eb6cf731f71186be719430
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-09-16 19:58:31 +0200

    Using CMAKE for linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,20 +7,19 @@ fi
 
 export LIBRARY_PATH="${PREFIX}/lib"
 
-./configure --prefix="${PREFIX}" \
-            --with-pic \
-            --enable-linux-lfs \
-            --with-zlib="${PREFIX}" \
-            --with-pthread=yes  \
-            --enable-cxx \
-            --enable-fortran \
-            --enable-fortran2003 \
-            --with-default-plugindir="${PREFIX}/lib/hdf5/plugin" \
-            --enable-threadsafe \
-            --enable-shared \
-            --enable-build-mode=production \
-            --enable-unsupported \
-            --with-ssl
+cmake -G $CMAKE_GENERATOR \
+      -D CMAKE_BUILD_TYPE:STRING=RELEASE \
+      -D CMAKE_PREFIX_PATH:PATH=$LIBRARY_PREFIX \
+      -D CMAKE_INSTALL_PREFIX:PATH=$LIBRARY_PREFIX \
+      -D HDF5_BUILD_CPP_LIB:BOOL=ON \
+      -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
+      -D BUILD_SHARED_LIBS:BOOL=ON \
+      -D HDF5_BUILD_HL_LIB:BOOL=ON \
+      -D HDF5_BUILD_TOOLS:BOOL=ON \
+      -D HDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON \
+      -D HDF5_ENABLE_THREADSAFE:BOOL=ON \
+      -D ALLOW_UNSUPPORTED:BOOL=ON \
+      $SRC_DIR
 
 make -j "${CPU_COUNT}"
 make check
