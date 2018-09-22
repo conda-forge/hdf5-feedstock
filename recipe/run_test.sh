54a4f454044fbe5f4327ab5ebef9aa571809516c
Anthony Scopatz <scopatz@gmail.com>
2018-09-22 14:05:41 -0400

    replace -show flag with --version

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,19 +3,19 @@ set -e
 
 # Test C compiler
 echo "Testing h5cc"
-h5cc -show
+h5cc --version
 h5cc h5_cmprss.c -o h5_cmprss
 ./h5_cmprss
 
 # Test C++ compiler
 echo "Testing h5c++"
-h5c++ -show
+h5c++ --version
 h5c++ h5tutr_cmprss.cpp -o h5tutr_cmprss
 ./h5tutr_cmprss
 
 # Test Fortran 90 compiler
 echo "Testing h5fc"
-h5fc -show
+h5fc --version
 h5fc h5_cmprss.f90 -o h5_cmprss
 ./h5_cmprss
 
