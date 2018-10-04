98008e8c963950d41ee774dabb7871986e995dc2
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-10-04 17:26:37 +0200

    set env variables and specified the prefix path

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,25 +1,30 @@
 # Stop on first error
 set -e
 
+export HDF5_CC="${CC}"
+export HDF5_CLINKER="${LD}"
+export HDF5_FC="${FC}"
+export HDF5_USE_SHLIB="yes"
+
 # Test C compiler
 echo "Testing h5cc"
 h5cc --version
-h5cc h5_cmprss.c -o h5_cmprss
+h5cc --prefix="${PREFIX}" h5_cmprss.c -o h5_cmprss
 ./h5_cmprss
 
 # Test C++ compiler
 echo "Testing h5c++"
 h5c++ --version
-h5c++ h5tutr_cmprss.cpp -o h5tutr_cmprss
+h5c++ --prefix="${PREFIX}" h5tutr_cmprss.cpp -o h5tutr_cmprss
 ./h5tutr_cmprss
 
 # Test Fortran 90 compiler
 echo "Testing h5fc"
 h5fc --version
-h5fc h5_cmprss.f90 -o h5_cmprss
+h5fc --prefix="${PREFIX}" h5_cmprss.f90 -o h5_cmprss
 ./h5_cmprss
 
 # Test Fortran 2003 compiler, note that the file has a 90 extension
 echo "Testing h5fc for Fortran 2003"
-h5fc compound_fortran2003.f90 -o compound_fortran2003
+h5fc --prefix="${PREFIX}" compound_fortran2003.f90 -o compound_fortran2003
 ./compound_fortran2003
