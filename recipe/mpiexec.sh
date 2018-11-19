54eb7ef7da57180719934dd3b4e701c41dad63a6
Min RK <benjaminrk@gmail.com>
2018-11-19 10:03:44 +0100

    set OMPI_MCA_btl=self,tcp
    shared-memory seems to have issues in ompi, at least on mac

--- recipe/mpiexec.sh
+++ recipe/mpiexec.sh
@@ -7,6 +7,7 @@ if [[ "$mpi" == "mpich" ]]; then
 fi
 
 if [[ "$mpi" == "openmpi" ]]; then
+    export OMPI_MCA_btl=self,tcp
     export OMPI_MCA_plm=isolated
     export OMPI_MCA_rmaps_base_oversubscribe=yes
     export OMPI_MCA_btl_vader_single_copy_mechanism=none
