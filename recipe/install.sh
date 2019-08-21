3bce6f8308036453a4562dc7ac14fdd442b4dace
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-08-21 10:00:05 -0500

    Update install.sh

--- recipe/install.sh
+++ recipe/install.sh
@@ -7,7 +7,7 @@ if [[ ${mpi} == "openmpi" && "$(uname)" == "Darwin" ]]; then
 exit 0
 EOF
 fi
-if [[ ! ${HOST} =~ .*powerpc64le.* ]]; then
+if [[ ("$target_platform" != "linux-ppc64le") && ("$target_platform" != "linux-aarch64") ]]; then
   # https://github.com/h5py/h5py/issues/817
   # https://forum.hdfgroup.org/t/hdf5-1-10-long-double-conversions-tests-failed-in-ppc64le/4077
   make check RUNPARALLEL="${RECIPE_DIR}/mpiexec.sh -n 2"
