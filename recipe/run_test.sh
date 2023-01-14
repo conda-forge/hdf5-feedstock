a85350a1fea46abbcccf8ba502916df5f830824d
Mark Harfouche <mark.harfouche@gmail.com>
2023-01-14 12:43:52 -0500

    show -> showconfig

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -10,19 +10,19 @@ else
     h5fc=h5fc
 fi
 echo "Testing $h5cc"
-$h5cc -show
+$h5cc -showconfig
 $h5cc h5_cmprss.c -o h5_cmprss
 ./h5_cmprss
 
 # Test C++ compiler
 echo "Testing h5c++"
-h5c++ -show
+h5c++ -showconfig
 h5c++ h5tutr_cmprss.cpp -o h5tutr_cmprss
 ./h5tutr_cmprss
 
 # Test Fortran 90 compiler
 echo "Testing $h5fc"
-$h5fc -show
+$h5fc -showconfig
 $h5fc h5_cmprss.f90 -o h5_cmprss
 ./h5_cmprss
 
