d8b88aae66f358018c0e05b5414cdf963093a40c
H. Vetinari <h.vetinari@gmx.com>
2024-08-04 20:04:43 +1100

    DEBUG: temporarily vendor flang compiler activation
    Partially reverts "let compiler activation do its job"
    
    This reverts commit aad938e31a080bef633dd91dd7fa260b4ed51694.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,12 @@ cd build
 :: Set environment variables.
 set HDF5_EXT_ZLIB=zlib.lib
 
+:: temporarily vendor flang compiler activation
+set "FC=flang-new"
+set "LD=lld-link.exe"
+set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld -I%LIBRARY_INC%"
+set "LDFLAGS=-Wl,-defaultlib:%CONDA_PREFIX%/lib/clang/19/lib/windows/clang_rt.builtins-x86_64.lib"
+
 set "CXXFLAGS=%CXXFLAGS% -LTCG"
 if "%mpi%"=="impi" (
   :: cmake generates syntax errors if there are backslashes in paths
