e26713292458afe0fc8e0ef21998fb3dfd15d752
krande <kristoffer_andersen@outlook.com>
2024-10-12 09:42:53 +0200

    sync with main and test with latest flang

--- recipe/bld-flang.bat
+++ recipe/bld-flang.bat
@@ -4,8 +4,24 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
+REM set clanglibdir=%CONDA_PREFIX:\=/%/lib/clang/19/lib/windows
+REM copy "%clanglibdir%\clang_rt.builtins-x86_64.lib" "%LIBRARY_LIB%\clang_rt.builtins.lib"
+
 :: Set environment variables.
 set HDF5_EXT_ZLIB=zlib.lib
+
+:: temporarily vendor flang compiler activation
+set "FC=flang-new"
+set "CC=clang-cl"
+set "CXX=clang-cl"
+
+:: need to read clang version for path to compiler-rt
+FOR /F "tokens=* USEBACKQ" %%F IN (`clang.exe -dumpversion`) DO (
+    SET "CLANG_VER=%%F"
+)
+set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=x86_64-pc-windows-msvc"
+set "LDFLAGS=--target=x86_64-pc-windows-msvc -fms-runtime-lib=dll -fuse-ld=lld"
+set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%BUILD_PREFIX%/Library/lib/clang/!CLANG_VER:~0,2!/lib/windows/clang_rt.builtins-x86_64.lib"
 
 
 set "CXXFLAGS=%CXXFLAGS% -LTCG"
@@ -31,6 +47,7 @@ cmake -G "Ninja" ^
       -D CMAKE_BUILD_TYPE:STRING=RELEASE ^
       -D CMAKE_PREFIX_PATH:PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+      -D CMAKE_Fortran_COMPILER:STRING=%FC% ^
       -D HDF5_BUILD_CPP_LIB:BOOL=ON ^
       -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
       -D BUILD_SHARED_LIBS:BOOL=ON ^
@@ -38,6 +55,7 @@ cmake -G "Ninja" ^
       -D ONLY_SHARED_LIBS:BOOL=ON ^
       -D HDF5_BUILD_HL_LIB:BOOL=ON ^
       -D HDF5_BUILD_TOOLS:BOOL=ON ^
+      -D HDF5_BUILD_FORTRAN:BOOL=ON ^
       -D HDF5_BUILD_HL_GIF_TOOLS:BOOL=ON ^
       -D HDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON ^
       -D HDF5_ENABLE_THREADSAFE:BOOL=ON ^
@@ -45,12 +63,13 @@ cmake -G "Ninja" ^
       -D HDF5_ENABLE_SZIP_SUPPORT=ON ^
       -D ALLOW_UNSUPPORTED:BOOL=ON ^
       %SRC_DIR%
+
 if errorlevel 1 (
   dir CMakeFiles
-  type CMakeFiles/CMakeOutput.log
-  type CMakeFiles/CMakeError.log
-  type CMakeFiles/CMakeConfigureLog.yaml
-  exit 1
+  type CMakeFiles\CMakeOutput.log
+  type CMakeFiles\CMakeError.log
+  type CMakeFiles\CMakeConfigureLog.yaml
+  exit /b 1
 )
 
 :: Build C libraries and tools.
--- recipe/build.sh
+++ recipe/build.sh
@@ -156,5 +156,5 @@ if [[ ("$target_platform" != "linux-ppc64le") && \
       ("$target_platform" != "osx-arm64") ]]; then
   # https://github.com/h5py/h5py/issues/817
   # https://forum.hdfgroup.org/t/hdf5-1-10-long-double-conversions-tests-failed-in-ppc64le/4077
-  make check RUNPARALLEL="mpiexec -n 3"
+  make check RUNPARALLEL="mpiexec -n 2"
 fi
