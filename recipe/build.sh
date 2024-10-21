b3b974df2b0a355663be745947af4f7688b190a3
Sam Gillingham <gillingham.sam@gmail.com>
2024-10-22 09:28:31 +1000

    use PREFIX rather than CONDA_PREFIX. xylar's changes

--- recipe/build.sh
+++ recipe/build.sh
@@ -160,7 +160,7 @@ if [[ ("$target_platform" != "linux-ppc64le") && \
 fi
 
 # test for hdf5 C++ exceptions
-$CXX $RECIPE_DIR/testhdf5exc.cpp -o testhdf5exc -L$CONDA_PREFIX/lib -lhdf5_cpp -lhdf5 -I$CONDA_PREFIX/include
-LD_LIBRARY_PATH=$CONDA_PREFIX/lib DYLD_LIBRARY_PATH=$CONDA_PREFIX/lib ./testhdf5exc
+$CXX $RECIPE_DIR/testhdf5exc.cpp -o testhdf5exc -L$PREFIX/lib -lhdf5_cpp -lhdf5 -I$PREFIX/include
+LD_LIBRARY_PATH=$PREFIX/lib DYLD_LIBRARY_PATH=$PREFIX/lib ./testhdf5exc
 
 
--- recipe/testhdf5exc.cpp
+++ recipe/testhdf5exc.cpp
@@ -6,19 +6,21 @@ int main()
 {
     H5::Exception::dontPrint();
     
-    H5::H5File *keaImgH5File = new H5::H5File("/tmp/test.hd5", H5F_ACC_TRUNC, H5::FileCreatPropList::DEFAULT);
-    keaImgH5File->createGroup("/HEADER");
+    H5::H5File *H5File = new H5::H5File("/tmp/test.hd5", H5F_ACC_TRUNC, H5::FileCreatPropList::DEFAULT);
+    H5File->createGroup("/HEADER");
     
     H5::DataSet ds;
     try
     {
         // should fail
-        ds = keaImgH5File->openDataSet("/HEADER/NUMBANDS");
+        ds = H5File->openDataSet("/HEADER/NUMBANDS");
         std::cout << "Was able to open dataset\n";
+        return -1;
     }
     catch (const H5::Exception &e)
     {
         std::cout << "Failed to open dataset (expected)\n";
+        return 0;
     }
     
     return 0;
