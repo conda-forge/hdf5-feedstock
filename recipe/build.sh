5334645d00684a4606bb1fee29f71de7b46b62d1
Mark Harfouche <mark.harfouche@gmail.com>
2025-03-20 21:47:55 -0400

    Build DIRECT_VFD on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,12 @@ else
   HDF5_ENABLE_PARALLEL=OFF
 fi
 
+if [[ "$target_platform" == linux-* ]]; then
+  HDF5_ENABLE_DIRECT_VFD=ON
+else
+  HDF5_ENABLE_DIRECT_VFD=OFF
+fi
+
 # Configure step
 cmake $CMAKE_ARGS \
       -D CMAKE_BUILD_TYPE:STRING=RELEASE \
@@ -30,7 +36,33 @@ cmake $CMAKE_ARGS \
       -D ALLOW_UNSUPPORTED:BOOL=ON \
       -D HDF5_TEST_EXAMPLES:BOOL=OFF \
       -D HDF5_ENABLE_PARALLEL:BOOL=${HDF5_ENABLE_PARALLEL} \
+      -D HDF5_ENABLE_DIRECT_VFD=${HDF5_ENABLE_DIRECT_VFD} \
       ..
 
 make -j${CPU_COUNT}
 make install
+
+if [[ ${mpi} == "mpich" || (${mpi} == "openmpi" && "$(uname)" == "Darwin") ]]; then
+  # ph5diff hangs on darwin with openmpi, skip the test
+  # ph5diff also crashes on mpich 4.1
+  echo <<EOF > tools/test/h5diff/testph5diff.sh
+#!/bin/sh
+exit 0
+EOF
+fi
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1"  ]]; then
+    if [[ ${mpi} == "mvapich" ]]; then
+        # Setting environment variables to allow oversubscription
+        export MV2_ENABLE_AFFINITY=0
+        # Run tests excluding specific ones using ctest
+        ctest -E "(t_bigio|t_pmulti_dset|t_filters_parallel|t_cache_image)"
+    else
+        # Run parallel tests on native platforms
+        # https://github.com/h5py/h5py/issues/817
+        # https://forum.hdfgroup.org/t/hdf5-1-10-long-double-conversions-tests-failed-in-ppc64le/4077
+        make check RUNPARALLEL="mpiexec -n 2"
+    fi
+elif [[ "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    make check
+fi
