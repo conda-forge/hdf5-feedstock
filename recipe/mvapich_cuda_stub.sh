d3772771ee5ee033a94a63ad3fc86b7a306eaa97
Jean Iaquinta <jeani@freebio4.hpc.uio.no>
2025-10-28 23:04:36 +0100

    Fix CUDA_TARGET for aarch64

--- recipe/mvapich_cuda_stub.sh
+++ recipe/mvapich_cuda_stub.sh
@@ -6,7 +6,7 @@ export MPIR_CVAR_ENABLE_GPU=0
 
 export CUDA_HOME="${PREFIX}/targets/x86_64-linux"
 if [[ "${target_platform}" == "linux-aarch64" ]]; then
-  export CUDA_HOME="${PREFIX}/targets/aarch64-linux"
+  export CUDA_HOME="${PREFIX}/targets/sbsa-linux"
 fi
 export LD_LIBRARY_PATH="${CUDA_HOME}/lib/stubs:${LD_LIBRARY_PATH:-}"
 ln -sf "${CUDA_HOME}/lib/stubs/libcuda.so" "${CUDA_HOME}/lib/stubs/libcuda.so.1" || true
