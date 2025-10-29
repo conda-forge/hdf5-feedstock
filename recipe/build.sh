bf5176b7215679e8fde26014a7b9f17c3c51152c
Jean Iaquinta <44261743+j34ni@users.noreply.github.com>
2025-10-29 17:27:59 +0100

    Update build.sh
    Fix tabs

--- recipe/build.sh
+++ recipe/build.sh
@@ -114,7 +114,7 @@ done
             --with-pic \
             --host="${HOST}" \
             --build="${BUILD}" \
-	    --with-zlib="${PREFIX}" \
+	        --with-zlib="${PREFIX}" \
             --with-szlib="${PREFIX}" \
             --with-pthread=yes  \
             ${HDF5_OPTIONS} \
